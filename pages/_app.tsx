8876d2ee313b82d0747de1755ee84d26e7f29982
Rajneesh <rajneesh.k@codebrewinnovations.com>
2025-02-14 11:37:55 +0530

    font update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout';
-import { Inter } from 'next/font/google'
+import { Inter, Righteous } from 'next/font/google'
 
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
 })
+const righteous = Righteous({
+  weight: '400',
+  subsets: ['latin'],
+})
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main className={inter.className}>
+    <main className={righteous.className}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
